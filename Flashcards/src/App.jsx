8831f75ee944365953e4a4815fd261f1a4603597
Teklemya <Teklemya@mail.uc.edu>
2024-09-30 20:44:39 -0500

    connect flash deck

--- Flashcards/src/App.jsx
+++ Flashcards/src/App.jsx
@@ -1,16 +1,13 @@
-/* eslint-disable no-unused-vars */
-import { useState } from 'react'
 import './App.css'
 import FlashcardDeck from './Flashcards/Flashcards'
 function App() {
-
-  const [count, setCount] = useState(0)
 
   return (
     <div className="header">
         <h1>Med Quiz</h1>
         <h3>Test your knowledge of your medications</h3>
-        <h3>Count: {count}</h3>
+        <h3>Count: 40</h3>
+        
       <FlashcardDeck />
     </div>
   
